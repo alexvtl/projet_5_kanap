37dbab26fa44415ca74584310a21cd26e8386536
alexvtl <alexandre.vital@hotmail.fr>
2022-11-17 12:57:01 +0000

    affichage des kanaps en js

---
+++ front/js/script.js
@@ -0,0 +1,32 @@
+let items = document.getElementById('items')
+
+function createkanap(kanap){
+let newA = document.createElement('a');
+newA.setAttribute('href',`./product.html?id=${kanap._id}`)
+newA.innerHTML = `
+<article>
+<img src=${kanap.imageUrl} alt=${kanap.altTxt}>
+<h3 class="productName">${kanap.name}</h3>
+<p class="productDescription">${kanap.description}</p>
+</article>`
+return newA
+}
+
+
+fetch('http://localhost:3000/api/products')
+.then( async function(res){
+    if(res.ok){
+        return res.json()}
+})
+.then( function(value){
+    console.log(value)
+  for(let article of value){
+    console.log(article)
+    items.append(createkanap(article))
+  }
+   }
+)
+.catch(function(err) {
+    items.style.backgroundColor="red"
+   items.innerHTML="Impossible de charger les produits"
+})
